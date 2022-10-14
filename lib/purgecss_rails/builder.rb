c0cc86d1c3ef339e8bd5ba347a401931da1b9a41
Mitchell Gould <mitchellgould7@gmail.com>
2022-10-14 09:38:32 -0400

    add option to purge  unused keyframes  variables

--- lib/purgecss_rails/builder.rb
+++ lib/purgecss_rails/builder.rb
@@ -3,11 +3,14 @@ module PurgecssRails
     def initialize(purge_css_path:)
       @purge_css_executable = purge_css_path
       @css_files = []
+      @ignore_files = []
     end
 
-    def search_css_files(path, ignore: [])
+    def search_css_files(path, ignore: [], keyframes: false, variables: false)
       @css_files += Dir[path]
       @ignored_names = ignore
+      @keyframes = keyframes
+      @variables = variables
       self
     end
 
@@ -29,13 +32,21 @@ module PurgecssRails
       end
     end
 
+    def remove_keyframes
+      '--keyframes' if keyframes
+    end
+
+    def remove_variables
+      '--variables' if variables
+    end
+
     def optimize!
       print "\n"
       ignore_files.each do |f|
         print "purging #{f}\n"
       end
 
-      @result = `#{purge_css_executable} --css #{ignore_files.join(" ")} --content #{html_files_match.join(" ")}`
+      @result = `#{purge_css_executable} --css #{ignore_files.join(" ")} --content #{html_files_match.join(" ")} #{remove_keyframes} #{remove_variables}`
       @result = JSON.parse(result)
 
       result.each do |result_item|
@@ -62,6 +73,6 @@ module PurgecssRails
 
     private
 
-    attr_reader :purge_css_executable, :css_files, :html_files_match, :result
+    attr_reader :purge_css_executable, :css_files, :html_files_match, :result, :keyframes, :variables
   end
 end
